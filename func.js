f174a9839e293c9d455c27eaee7b8f3fc44f389c
shamim <shamimmahmud077@gmail.com>
2024-12-18 22:05:31 +0600

    types of function

--- func.js
+++ func.js
@@ -1,5 +1,24 @@
-function testFunc() {
-  console.log("i am function");
+function add(x, y) {
+  const total = x + y;
+  return total;
+}
+const z = add(5, 6);
+console.log(z);
+
+// types of function
+
+function fun1() {
+  console.log("on parameters, not return");
 }
 
-testFunc();
+function fun2() {
+  return 5;
+}
+
+function func3(name) {
+  console.log(name);
+}
+
+function func4(x, y) {
+  return x + y;
+}
