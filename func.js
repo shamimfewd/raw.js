98f9c878e1d42ee13874fae7fde02a1ea6b8343b
shamim <shamimmahmud077@gmail.com>
2024-12-19 21:51:45 +0600

    learning array

--- func.js
+++ func.js
@@ -1,24 +1,47 @@
-function add(x, y) {
-  const total = x + y;
-  return total;
-}
-const z = add(5, 6);
-console.log(z);
+// function add(x, y) {
+//   const total = x + y;
+//   return total;
+// }
+// const z = add(5, 6);
+// // console.log(z);
 
-// types of function
+// // types of function
 
-function fun1() {
-  console.log("on parameters, not return");
-}
+// function fun1() {
+//   console.log("on parameters, not return");
+// }
 
-function fun2() {
-  return 5;
-}
+// function fun2() {
+//   return 5;
+// }
 
-function func3(name) {
-  console.log(name);
-}
+// function func3(name) {
+//   console.log(name);
+// }
 
-function func4(x, y) {
-  return x + y;
+// function func4(x, y) {
+//   return x + y;
+// }
+
+// const x = 5;
+// if (x % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function myFunc(arr) {
+//   return arr;
+// }
+
+// const result = myFunc(arr);
+// console.log(result.length);
+
+const arr = [1, 2, 3, 6, 7, 5, 65, 8, 9];
+if (arr.length >= 10) {
+  console.log("ok");
+} else {
+  console.log("not ok");
 }
