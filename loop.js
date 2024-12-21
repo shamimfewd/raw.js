3605ae3e83276bf80a242aeacd88591dc6add0f1
shamim <shamimmahmud077@gmail.com>
2024-12-21 16:47:27 +0600

    leson of for loop

--- loop.js
+++ loop.js
@@ -7,8 +7,33 @@
 
 // 'while' loop
 
-let num = 0;
-while (num < 5) {
-    // console.log("loop = ", num + 1);
-    num++;
+// let num = 51;
+// while (num <= 100) {
+//   console.log("loop = ", num);
+//   num++;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numbers2 = [];
+// for (const num of numbers) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//     numbers2.push(num);
+//   }
+// }
+// console.log(numbers);
+// console.log(numbers2);
+
+// let number = 5;
+// let range = 10;
+// let i = 1;
+
+// while (i <= range) {
+//   console.log(`${number * i}`);
+//   i++;
+// }
+
+// ============= for loop =============
+for (let i = 11; i >= 1; i--) {
+  console.log(i);
 }
