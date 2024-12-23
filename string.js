eab31a1d6d9606158e8e2f68ddf9e4b39e96975c
shamim <shamimmahmud077@gmail.com>
2024-12-23 07:47:00 +0600

    leson of string

---
+++ string.js
@@ -0,0 +1,9 @@
+const address = "mohammadpur";
+// console.log(address.slice(2, 5));
+// console.log(address.split(" "));
+// console.log(address.substring());
+
+const add = "my, home, town, is, gaibandha";
+// console.log(add.concat(" ").concat(address));
+// console.log(add.join());
+// console.log(add.includes("s"));
