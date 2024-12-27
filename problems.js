188b924735a2f686c49ef94762606419ae7f53e5
shamim <shamimmahmud077@gmail.com>
2024-12-27 09:41:47 +0600

    find the avarage number from an array

--- problems.js
+++ problems.js
@@ -1,7 +1,30 @@
-function gramToLiter(gram) {
-  const liter = gram / 1000;
-  return liter;
-}
+// function gramToLiter(gram) {
+//   const liter = gram / 1000;
+//   return liter;
+// }
 
-const result = gramToLiter(1500);
-console.log(`${result} ${result > 1 ? "liters" : "liter"}`);
+// const result = gramToLiter(1500);
+// console.log(`${result} ${result > 1 ? "liters" : "liter"}`);
+
+// ==============================
+
+function oddNumbers(numbers) {
+  const odds = [];
+  for (const number of numbers) {
+    if (number % 2 === 1) {
+      odds.push(number);
+    }
+  }
+
+  let sum = 0;
+  for (const odd of odds) {
+    sum = sum + odd;
+  }
+
+  const avg = sum / odds.length;
+
+  return avg;
+}
+const numbers = [2, 5, 9, 6, 8, 7, 54, 63, 1, 4, 6, 2, 4, 55];
+const result = oddNumbers(numbers);
+console.log(result.toFixed(2));
