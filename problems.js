fd4ff993fc867dd87abddf9800432da921e0f6b2
shamim <shamimmahmud077@gmail.com>
2024-12-28 20:04:08 +0600

    leson of problem solving

--- problems.js
+++ problems.js
@@ -8,23 +8,54 @@
 
 // ==============================
 
-function oddNumbers(numbers) {
-  const odds = [];
-  for (const number of numbers) {
-    if (number % 2 === 1) {
-      odds.push(number);
-    }
-  }
+// function oddNumbers(numbers) {
+//   const odds = [];
+//   for (const number of numbers) {
+//     if (number % 2 === 1) {
+//       odds.push(number);
+//     }
+//   }
 
-  let sum = 0;
-  for (const odd of odds) {
-    sum = sum + odd;
-  }
+//   let sum = 0;
+//   for (const odd of odds) {
+//     sum = sum + odd;
+//   }
 
-  const avg = sum / odds.length;
+//   const avg = sum / odds.length;
 
-  return avg;
+//   return avg;
+// }
+// const numbers = [2, 5, 9, 6, 8, 7, 54, 63, 1, 4, 6, 2, 4, 55];
+// const result = oddNumbers(numbers);
+// console.log(result.toFixed(2));
+
+// ==================================
+
+// const numbers = [
+//   1, 2, 5, 8, 7, 9, 6, 5, 4, 2, 3, 1, 2, 3, 6, 5, 4, 7, 8, 9, 3, 2, 1, 4, 7, 8,
+//   9, 6, 3, 2,
+// ];
+
+// function uniqueArray(numbers) {
+//   const unique = [];
+//   for (const number of numbers) {
+//     if (unique.includes(number) === false) {
+//       unique.push(number);
+//     }
+//   }
+//   return unique;
+// }
+
+// const result = uniqueArray(numbers);
+// console.log(result);
+
+// =================================
+
+const numbers = [5, 6, 11, 12, 98, 5];
+
+function findMultiple(numbers, num) {
+  const count = numbers.filter((item) => item === num).length;
+  return count;
 }
-const numbers = [2, 5, 9, 6, 8, 7, 54, 63, 1, 4, 6, 2, 4, 55];
-const result = oddNumbers(numbers);
-console.log(result.toFixed(2));
+const result = findMultiple(numbers, 2525);
+console.log(result);
